3dff9252f6c419035647ed91bb6fd3f778c46908
SantiagoLaraE <oscarescobar773@gmail.com>
2023-08-16 13:26:38 -0500

    Sequelize connection and test

---
+++ index.js
@@ -0,0 +1,12 @@
+const sequelize = require("./libs/sequelize");
+
+async function TestConnection() {
+  try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+}
+
+TestConnection()
