4b5de98c2705296fd37018e815e4682fca6494dd
SantiagoLaraE <oscarescobar773@gmail.com>
2023-08-17 14:01:53 -0500

    Setup cors and whitelist

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
 const express = require('express');
+const cors = require('cors');
 const config = require('./config');
 const routerApi = require('./routes');
 const {
   boomErrorHandler,
   errorHandler,
-  sequelizeErrorHandler
+  sequelizeErrorHandler,
 } = require('./middlewares/error.handler');
 
 const app = express();
 app.use(express.json());
+
+const whitelist = [
+  'http://localhost',
+  'https://hola-store.vercel.app',
+];
+const options = {
+  origin: (origin, callback) => {
+    if (whitelist.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('no permitido'));
+    }
+  },
+};
+
+app.use(cors(options));
 
 routerApi(app);
 app.use(boomErrorHandler);
