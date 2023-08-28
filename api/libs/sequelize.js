dc277bb7e2d5c9b359efc1a0dfc4c7bf15f964a5
SantiagoLaraE <oscarescobar773@gmail.com>
2023-08-28 10:31:30 -0500

    Fiz sequelize ssl connection

--- api/libs/sequelize.js
+++ api/libs/sequelize.js
@@ -4,10 +4,11 @@ const setupModels = require('../db/models');
 
 const USER = encodeURIComponent(config.dbUser);
 const PASSWORD = encodeURIComponent(config.dbPassword);
-const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}?sslmode=require`;
 
 const sequelize = new Sequelize(URI, {
   dialect: 'postgres',
+  sslmode: 'require',
   logging: console.log,
 });
 
